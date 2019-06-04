b72ae179fc032131da582c20fd78e9b0199bf480
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-04 16:14:59 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,4 +19,12 @@ class Post < ActiveRecord::Base
 
     errors.add(:title, "must be clickbait")
   end
+   FactoryBot.define do
+    factory :post do
+      title {"Secret"}
+      content {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat."}
+      summary {"Awesome"}
+      category {"Fiction"}
+    end
+  end
 end
